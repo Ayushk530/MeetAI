708400084178193703a620ae72ed13390f5d36e5
Ayush Khanuja <khanujaayush555@gmail.com>
2025-09-11 23:01:24 +0530

    meeting setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { createTRPCRouter } from '../init';
 import { agentsRouter } from '@/modules/agents/server/procedure';
-import { agents } from '@/db/schema';
+import { agents, meetings } from '@/db/schema';
+import { meetingsRouter } from '@/modules/meetings/server/procedure';
 export const appRouter = createTRPCRouter({
   agents:agentsRouter,
+  meetings:meetingsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
