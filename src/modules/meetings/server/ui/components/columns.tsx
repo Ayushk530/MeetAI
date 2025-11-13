a88989f61be2ed211a19a483ae31cd56af349bb5
Ayush Khanuja <khanujaayush555@gmail.com>
2025-11-13 22:45:15 +0530

    Completed States

--- src/modules/meetings/server/ui/components/columns.tsx
+++ src/modules/meetings/server/ui/components/columns.tsx
@@ -6,16 +6,9 @@ import { CornerDownRightIcon, VideoIcon,CircleCheckIcon,CircleXIcon,ClockArrowUp
 import { Badge } from "@/components/ui/badge"
 import { MeetingGetMany } from "@/modules/meetings/types"
 import { format } from "date-fns";
-import humanizeDuration from "humanize-duration";
-import { cn } from "@/lib/utils"
+import { cn, formatDuration } from "@/lib/utils"
 
-function formatDuration(seconds:number){
-    return humanizeDuration(seconds*1000,{
-        largest:1,
-        round:true,
-        units : ["h","m","s"],
-    });
-};
+
 
 const statusIconMap = {
     upcoming : ClockArrowUpIcon,
